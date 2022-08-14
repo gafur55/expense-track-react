07ff98254b8fd7622016e04e1f6b1970a2369ccb
gafur55 <g_mammadov@u.pacific.edu>
2022-08-14 13:42:17 -0700

    push trial

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,7 +5,7 @@ return (
      <h3>History</h3>
      <ul id='list' class='list'>
         <li class='minus'>
-            Cash <span>-$400</span><button class='delete-btn'>x</button>
+            Cash <span>-$300</span><button class='delete-btn'>x</button>
         </li>
      </ul>
     </>
